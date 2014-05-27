eff14d48f319f5b5cd4f659834ee859b5edc96bb
roberto_da_conceicao <conceicao.roberto@gmail.com>
2014-05-27 15:36:38 -0300

    Arquivos iniciais para o site

---
+++ js/app.js
@@ -0,0 +1,27 @@
+// create the module and name it negocieApp
+var negocieApp = angular.module('negocieApp', [
+	'ngRoute',
+	'negocieControllers'
+	]);
+
+// configure our routes
+negocieApp.config(function($routeProvider) {
+	$routeProvider
+		// route for the home page
+		.when('/', {
+			templateUrl : 'pages/home.html',
+			controller  : 'mainController'
+		})
+
+		// route for the about page
+		.when('/about', {
+			templateUrl : 'pages/about.html',
+			controller  : 'aboutController'
+		})
+
+		// route for the contact page
+		.when('/contact', {
+			templateUrl : 'pages/contact.html',
+			controller  : 'contactController'
+		});
+});
---
+++ js/controllers.js
@@ -0,0 +1,35 @@
+var negocieControllers = angular.module('negocieControllers', []);
+
+// create the controller and inject Angular's $scope
+negocieControllers.controller('mainController', function($scope) {
+	// create a message to display in our view
+	$scope.message = 'Everyone come and see how good I look!';
+});
+
+negocieControllers.controller('aboutController', function($scope) {
+	$scope.message = 'Look! I am an about page.';
+});
+
+negocieControllers.controller('contactController', function($scope) {
+	$scope.message = 'Contact us! JK. This is just a demo.';
+});
+
+/*
+negocieControllers.controller('PhoneListCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+    $http.get('phones/phones.json').success(function(data) {
+      $scope.phones = data;
+    });
+
+    $scope.orderProp = 'age';
+  }]);
+
+negocieControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http',
+  function($scope, $routeParams, $http) {
+	$http.get('phones/'+$routeParams.phoneId+'.json').success(
+		function(data){
+			$scope.phone = data;
+		}
+	);    
+  }]);
+*/
