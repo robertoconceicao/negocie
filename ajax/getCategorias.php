75935bfa6616f8955aa81d0f909e1314e15374b4
robertoconceicao <conceicao.roberto@gmail.com>
2014-06-05 20:46:47 -0300

    Adicionando novos arquivos

--- ajax/getCategorias.php
+++ ajax/getCategorias.php
@@ -1,9 +1,10 @@
 <?php 
 require_once 'db.php'; // The mysql database connection script
-$query=mysql_query(" SELECT * FROM categoria") or die(mysql_error());
+$query=mysql_query(" SELECT * FROM categoria where cdcategoriaparent is null ") or die(mysql_error());
 
 # Collect the results
 while($obj = mysql_fetch_object($query)) {
+	$obj->nmcategoria = utf8_encode($obj->nmcategoria);
     $arr[] = $obj;
 }
 
--- js/app.js
+++ js/app.js
@@ -1,9 +1,29 @@
+/*global angular */
+'use strict';
+
 // create the module and name it negocieApp
 var negocieApp = angular.module('negocieApp', [
 	'ngRoute',
-	'negocieControllers'
+	'negocieControllers',
+	'flow'
 	]);
 
+negocieApp.config(['flowFactoryProvider', function (flowFactoryProvider) {
+    flowFactoryProvider.defaults = {
+      target: '',
+      permanentErrors: [500, 501],
+	  
+      maxChunkRetries: 1,
+      chunkRetryInterval: 5000,
+      simultaneousUploads: 1
+    };
+    flowFactoryProvider.on('catchAll', function (event) {
+      console.log('catchAll', arguments);
+    });
+    // Can be used with different implementations of Flow.js
+    flowFactoryProvider.factory = fustyFlowFactory;
+  }]);
+  
 // configure our routes
 negocieApp.config(function($routeProvider) {
 	$routeProvider
