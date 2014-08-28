e26770779aba74b08bdd1e86d9a851af9999fca8
robertoconceicao <conceicao.roberto@gmail.com>
2014-08-27 21:27:20 -0300

    add post anuncio.php teste

---
+++ ajax/postAnuncio.php
@@ -0,0 +1,12 @@
+<?php
+	
+	echo "Titulo: ". $_POST["titulo"] . "<br>"; 
+	echo "Categoria: ". $_POST["categoria"] . "<br>";
+	echo "Descricao: ". $_POST["descricao"] . "<br>";
+	echo "Preco: ". $_POST["preco"] . "<br>";
+	echo "Nome: ". $_POST["nome"] . "<br>";
+	echo "Email: ". $_POST["email"] . "<br>";
+	echo "Telefone: ". $_POST["telefone"] . "<br>";
+	echo "Estado: ". $_POST["estado"] . "<br>";
+	echo "Cidade: ". $_POST["cidade"] . "<br>";
+?>
